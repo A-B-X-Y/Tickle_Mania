c712076e229aacbf6fac650642b0d3653bf4a3d0
A-B-X-Y <106481189+A-B-X-Y@users.noreply.github.com>
2022-10-18 12:50:48 +0600

    Add files via upload

---
+++ eternals.js
@@ -0,0 +1,21 @@
+function ep1(){
+    var videoEls = document.getElementsByTagName("iframe");
+        videoEls[0].src = "https://dood.wf/e/2ynqgbii8ff1y7svde3muz61gwd2y6of";
+    var epEls = document.getElementById("ep1");
+    epEls.className = "onpg";
+
+        var alep = document.getElementById("eph1");
+        alep.className = "vbox";
+
+   
+
+};
+function eph1(){
+    var videoEls = document.getElementsByTagName("iframe");
+        videoEls[0].src = "https://dood.wf/e/yycjvgf4vh0lhti31qslot15qd0zd00u";
+    var epEls = document.getElementById("eph1");
+    epEls.className = "onpg";
+
+    var alep = document.getElementById("ep1");
+    alep.className = "vbox";
+};

ce3a8fcebf9fed8ec29c0db9f87f26bec5c510ee
A-B-X-Y <106481189+A-B-X-Y@users.noreply.github.com>
2022-10-19 11:55:26 +0600

    Add files via upload

---
+++ joker.js
@@ -0,0 +1,12 @@
+function ep1(){
+    var videoEls = document.getElementsByTagName("iframe");
+        videoEls[0].src = "https://dood.wf/e/pp8jzr5k4c4y0djeb2bctwzlcpffcfxg";
+    var epEls = document.getElementById("ep1");
+    epEls.className = "onpg";
+
+        var alep = document.getElementById("eph1");
+        alep.className = "vbox";
+
+   
+
+};

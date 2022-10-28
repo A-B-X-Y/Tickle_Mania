2bcbb3022449fa229e1456782e0cbac562b02bbf
A-B-X-Y <106481189+A-B-X-Y@users.noreply.github.com>
2022-10-29 01:46:04 +0600

    Add files via upload

---
+++ raginmmsreturnss02.js
@@ -0,0 +1,234 @@
+function ep1(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/qu224qzzijfc5mcif3gpftod9kv79x37";
+    };
+    var epEls = document.getElementById("ep1");
+    epEls.className = "onpg";
+
+    var epar =["ep2","ep3","ep4","ep5","ep6","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+
+
+function ep2(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/msmj0zmsjxaspsz0vl2kx29u2nglgfkh";
+    };
+    var epEls = document.getElementById("ep2");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+
+
+};
+function ep3(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/zezanar6ypkjsfd30k0noi5tao964nj0";
+    };
+    var epEls = document.getElementById("ep3");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep2","ep4","ep5","ep6","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep4(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/xj9wz6e3l6c8kur1nluvelps7kv77rps";
+    };
+    var epEls = document.getElementById("ep4");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep2","ep5","ep6","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep5(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/0jjsby0qc4m6k9a3bxkz08ul8fy35dzi";
+    };
+    var epEls = document.getElementById("ep5");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep2","ep6","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep6(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/8kvyeunkwfrzm9vqsy3ohp7m3f03kvem";
+    };
+    var epEls = document.getElementById("ep6");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep2","ep7","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep7(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/f3l6re0krxakg498yta9w3pd5tfyuu5b";
+    };
+    var epEls = document.getElementById("ep7");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep2","ep8","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep8(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/r9h8w0dj6owid3qnegi12bl7whu3r8bc";
+    };
+    var epEls = document.getElementById("ep8");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep2","ep9","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep9(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/awxf2ne9jic1gpu1jl0ihlw901lv02xr";
+    };
+    var epEls = document.getElementById("ep9");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep10","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep10(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/74yymsij1kqyyzcma1mzww2pa2hh1frt";
+    };
+    var epEls = document.getElementById("ep10");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep9","ep11","ep12","ep13","ep14","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep11(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/nhy4v5n57qc2fdu45b5lob0bm1honx44";
+    };
+    var epEls = document.getElementById("ep11");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep9","ep10","ep12","ep14","ep13","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep12(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/530jrdov85neln3n5jratqzr0ejgmsin";
+    };
+    var epEls = document.getElementById("ep12");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep10","ep11","ep9","ep14","ep13","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep13(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/ujmqrvrmoxrq35ftxxia49e7ah8oakob";
+    };
+    var epEls = document.getElementById("ep13");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep10","ep11","ep9","ep14","ep12","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep14(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/kdrw9122j1hbgm67veywmtq534v7mdgc";
+    };
+    var epEls = document.getElementById("ep14");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep10","ep11","ep9","ep13","ep12","ep15"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+function ep15(){
+    var videoEls = document.getElementsByTagName("iframe");
+    for (var i = 0; i < videoEls.length; i++) {
+        videoEls[i].src = "https://dood.re/e/0tj9t35g9lus7zi1wyzqf411kr1kj0fk";
+    };
+    var epEls = document.getElementById("ep15");
+    epEls.className = "onpg";
+
+    var epar =["ep1","ep3","ep4","ep5","ep6","ep7","ep8","ep2","ep10","ep11","ep9","ep14","ep13","ep12"];
+    for (var i = 0; i < epar.length; i++){
+        var alep = document.getElementById(epar[i]);
+        alep.className = "vbox";
+
+    }
+};
+
+
+
+
+
